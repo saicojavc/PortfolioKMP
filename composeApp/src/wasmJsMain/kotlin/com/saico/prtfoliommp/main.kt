4f3fbbaa3023e4859fcd502e2ac81b26c6b84d28
Jorge Adrian Valdes Camacho <jorgito.avc03@gmail.com>
2025-08-27 16:56:16 -0400

    init commit

--- composeApp/src/wasmJsMain/kotlin/com/saico/prtfoliommp/main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/saico/prtfoliommp/main.kt
@@ -3,7 +3,7 @@ package com.saico.prtfoliommp
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
-
+//
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport(document.body!!) {
